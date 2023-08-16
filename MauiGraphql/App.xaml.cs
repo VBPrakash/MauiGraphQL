2469b5e8f99a27235a92a7e5ec7ab4dbd462874b
Vanka, Bhanu <Bhanu.vanka@daimlertruck.com>
2023-08-16 21:42:24 +0530

    First check in

---
+++ MauiGraphql/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiGraphql;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiGraphql/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiGraphql;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
